034e2ebe566447934cb46e12f4dd075f676366f7
AlejandroAlv12 <00211623@uca.edu.sv>
2025-11-23 15:00:54 -0600

    feat: agregar footer

--- odessa/frontend/src/App.jsx
+++ odessa/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Services /> 
+      <Footer />
     </>
   );
 }
